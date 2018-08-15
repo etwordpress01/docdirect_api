5ccbb5ca8295d57e8d7205ae4cd42f53ad48c4c6
usman_html03 <ethtml03@gmail.com>
2018-08-15 12:14:50 +0500

    subcatogry

--- lib/categories.php
+++ lib/categories.php
@@ -51,43 +51,34 @@ if (!class_exists('DocdirectAppCategoryRoutes')) {
                 'post_status' => 'publish',
                 'suppress_filters' => false
             );
-
-            $options = '';
+			$options = '';
             $cust_query = get_posts($args);
 			$items	= array();
             if (!empty($cust_query)) {
                 $counter = 0;
                 foreach ($cust_query as $key => $dir) {
                     $meta = get_post_meta($dir->ID);
-                    $item = array();
+					$item = array();
 
                     $item['id'] 	= $dir->ID;
                     $item['title']  = get_the_title($dir->ID);
 
                     $item += unserialize($meta['fw_options'][0]);
+					$specialities = $item['specialities'];
+	
+					if (!empty($specialities)) {
+						$subarray = array();
+                        foreach ($specialities as $key => $term) {
+							$speciality = get_term_by('id',$key,'specialities','OBJECT');
+							$subarray[] = $speciality;
+						}
 
-
-                    /*if (isset($dir->ID)) {
-                        $sub_categories = wp_get_post_terms($dir->ID,
-                                'sub_category', array("fields" => "all"));
-
-                        $subarray = array();
-                        if (!empty($sub_categories)) {
-                            foreach ($sub_categories as $key => $sub_category) {
-                                if (!empty($sub_category)) {
-                                    $sub['slug'] = $sub_category->slug;
-                                    $sub['title'] = htmlspecialchars_decode($sub_category->name);
-                                }
-                                $subarray[] = $sub;
-                            }
-                        }
-
-                        $item['sub_categories'] = $subarray;
-                    }*/
+                        $item['specialities'] = $subarray;
+                    } 
 					
 					$items[] = $item;
                 }
-            }
+			} 
 
             return new WP_REST_Response($items, 200);
         }
